88004afb7474c407901f3406b8b64502d469b209
AlanKostrick <31017085+AlanKostrick@users.noreply.github.com>
2019-09-30 12:11:41 -0500

    Initialize GradingFeedback

---
+++ src/main/java/org/wcci/reviewssite/ReviewsSiteApplication.java
@@ -0,0 +1,13 @@
+package org.wcci.reviewssite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewsSiteApplication.class, args);
+	}
+
+}
