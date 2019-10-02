630a5eb68ce2d7d2e709762745224f237046b51c
Brendin Headlee <Brendin.Headlee24@gmail.com>
2019-10-02 10:31:21 -0400

    added hashmap

---
+++ src/main/java/org/wcci/reviewssite/ReviewRepo.java
@@ -0,0 +1,41 @@
+package org.wcci.reviewssite;
+
+import java.util.HashMap;
+
+public class ReviewRepo {
+
+	private HashMap<String,Review> reviews; 
+	
+	
+	public ReviewRepo() {
+		reviews = new HashMap<>();
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+
+
+
+	public void addReview(Review testReview) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+	public HashMap<String,Review> getReviews() {
+		// TODO Auto-generated method stub
+		return reviews;
+	}
+
+}
---
+++ src/test/java/org/wcci/reviewssite/ReviewRepoTest.java
@@ -0,0 +1,18 @@
+package org.wcci.reviewssite;
+
+import org.junit.Test;
+
+public class ReviewRepoTest {
+	@Test 
+	public void shouldInstantiate() {
+		ReviewRepo underTest= new ReviewRepo(); 
+	}
+	@Test
+	public void shouldAddReview() { 
+		ReviewRepo underTest= new ReviewRepo();
+		Review testReview= new Review("Id","String","String",5,"String","String");
+		underTest.addReview(testReview);
+		assertTrue(underTest.getReviews().containsKey("Id"));
+	}
+
+}
